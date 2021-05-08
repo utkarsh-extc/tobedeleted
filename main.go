3ba4f1f19065ad1f3accec0599fdc0652141abfe
Utkarsh Dixit <utkarsh.extc@gmail.com>
2021-05-08 13:59:54 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"go/src/io/ioutil"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
+	"strings"
 	"time"
 )
 
@@ -43,19 +44,25 @@ type Centers struct {
 var pinCode = ""
 var emailHost = "smtp.gmail.com"
 var emailFrom = ""
+var emailRecipients=[]string{""}
 var emailPassword = ""
-var duration = 1 * time.Minute
+// api limit 100 req/5min per ip
+var duration = 5*time.Minute/95
 var count int
 
 func main() {
-	flag.StringVar(&pinCode, "districtCode", "363", "pincode of your area\ne.g 363 for pune")
+	list:=""
+	flag.StringVar(&pinCode, "pincode", "411027", "pincode of your area")
 	flag.StringVar(&emailFrom, "emailId", emailFrom, "your_id@gmail.com")
+	flag.StringVar(&list,"to",emailFrom,"comma separated email list")
 	flag.StringVar(&emailPassword, "password", emailPassword, "Google App Password \ncreate new using https://support.google.com/accounts/answer/185833")
 	flag.DurationVar(&duration, "interval", duration, "time interval as duration \ne.g. 30s,5m,1h")
 
 	flag.Parse()
 
-	if pinCode == "" || emailPassword == "" || emailFrom == "" {
+	emailRecipients=strings.Split(list,",")
+
+	if pinCode == "" || emailPassword == "" || emailFrom == "" || len(emailRecipients)<1 {
 		flag.Usage()
 		return
 	}
@@ -76,7 +83,7 @@ loop:
 			now := time.Now()
 			date := fmt.Sprintf("%02d-%02d-%04d", now.Day(), now.Month(), now.Year())
 			req, _ := http.NewRequest(http.MethodGet,
-				"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+pinCode+"&date="+date, nil)
+				"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pinCode+"&date="+date, nil)
 
 			req.Header.Set("cache-control", "no-cache")
 			req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/90.0.4430.93 Safari/537.36")
@@ -131,8 +138,9 @@ loop:
 
 					// Connect to the server, authenticate, set the sender and recipient,
 					// and send the email all in one step.
-					to := []string{emailFrom}
-					msg := []byte("To: " + emailFrom + "\r\n" +
+					to := emailRecipients
+					msg := []byte("To: " + list + "\r\n" +
+						"From: Golang looper\r\n" +
 						"Subject: Available Centers!\r\n" +
 						"\r\n" +
 						"List : \r\n" +
@@ -145,7 +153,7 @@ loop:
 					count++
 				}()
 
-				time.Sleep(10 * time.Minute)
+				time.Sleep(5 * time.Minute)
 				if count > 10 {
 					break loop
 				}
